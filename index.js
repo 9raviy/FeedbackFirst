abd482bde186337ab9e3e458d94212c11cf9d503
Ravi Yadav <9raviy@iima.ac.in>
2020-11-26 12:14:43 -0500

    changed data object

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const app= express()
 
-app.get('/',(req,res)=>{res.send({'name':'Ravi','Age':'20'})}
+app.get('/',(req,res)=>{res.send({'name':'Ravi','Age':'20',"Sex":"Male"})}
     )
 
 const PORT= process.env.PORT || 5000
